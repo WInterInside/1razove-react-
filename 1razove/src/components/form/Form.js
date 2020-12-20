08b459b6f61c95e79708595da2bd14c91bc5198e
Dmytro Titov <dmitry90titov@gmail.com>
2020-12-20 19:50:20 +0200

    form working

--- 1razove/src/components/form/Form.js
+++ 1razove/src/components/form/Form.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Form.scss";
+import axios from 'axios';
 
 export default function Form({closePopup}) {
+  let [email, setEmail] = useState('');
+  let [name, setName] = useState('');
 
   function close(){
     closePopup();
@@ -12,20 +15,34 @@ export default function Form({closePopup}) {
     e.nativeEvent.stopImmediatePropagation();
   }
 
+  function onNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function onEmailChange (e) {
+    setEmail(e.target.value);
+  }
+
+  function submit(){
+    let url = `https://crm.razove.com.ua/rest/1/tmz971rk5etxiy2j/crm.lead.add.json?FIELDS[TITLE]=Новий лід&FIELDS[NAME]=${name}&FIELDS[EMAIL][0][VALUE]=${email}&FIELDS[EMAIL][0][VALUE_TYPE]=WORK&FIELDS[UTM_SOURCE]=fb&FIELDS[UTM_CAMPAIGN]=123&FIELDS[UTM_CONTENT]=123&FIELDS[UTM_MEDIUM]=123&FIELDS[UTM_TERM]=123`;
+    axios.get(url);
+    close();
+  }
+
   return (
     <section className="form container container-padding0" onClick={(e) => stopPropagation(e)}>
       <p className="form__text form__text--main">Отримати прайс-лист продуктів Maxtec</p>
       <p className="form__text">Введіть ваші дані і ми відправимо вам прайс-лист на пошту.</p>
-      <form action="https://echo.htmlacademy.ru" method="post">
+      <form>
         <div className="form__wrapper">
           <label className="form__label visually-hidden" for="name"> Ваше ім’я / Назва організації </label>
-          <input className="form__input" type="text" name="name" id="name" placeholder="Ваше ім’я / Назва організації" required />
+          <input  onChange={onNameChange} className="form__input" type="text" name="name" id="name" placeholder="Ваше ім’я / Назва організації" required />
         </div>
         <div className="form__wrapper">
           <label className="form__label visually-hidden" for="email"> Email </label>
-          <input className="form__input" type="text" name="name" id="email" placeholder="Email" required />
+          <input onChange={onEmailChange} className="form__input" type="text" name="name" id="email" placeholder="Email" required />
         </div>
-        <input className="form__submit button button--submit" type="submit" name="submit" value="Отримати прайс-лист" />
+        <input onClick={submit} className="form__submit button button--submit" name="submit" value="Отримати прайс-лист" />
         <div className="form__toggle" onClick={() => close()}></div>
       </form>
     </section>
