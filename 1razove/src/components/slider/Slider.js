1aa33edba18e9db5c04dff1591c39537434b77d0
Dmytro Titov <dmitry90titov@gmail.com>
2020-12-21 09:53:02 +0200

    popup on slider

--- 1razove/src/components/slider/Slider.js
+++ 1razove/src/components/slider/Slider.js
@@ -1,39 +1,53 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import {tns} from 'tiny-slider/src/tiny-slider';
 import "./Slider.scss";
+import Overlay from '../overlay/Overlay';
 
+let initialized = false;
 export default function Slider({data}) {
+  let [selectedImg, setSelectedImage] = useState('');
+  let [showPopup, setShowPopup] = useState(false);
 
-  useEffect (
-    () => {
-      setTimeout (
-        () => {
-          var slider = tns({
-            container: '.product__slider',
-            items: 2,
-            autoWidth: true,
-            gutter: 10,
-            controls: true,
-            nav: false,
-            autoplay: false,
-            mouseDrag: true,
-            arrowKeys: true,
-            controlsContainer: '.product__toggls',
-            prevButton: '.product__arrow--prev',
-            nextButton: '.product__arrow--next',
-            responsive: {
-              900: {
-                items: 4,
-                gutter: 15
-              }
-            }
-          });
-        },
-        1000
-      )
-    }
-  );
+  function openOverlay(url){
+    console.log(url);
+    setSelectedImage(url);
+    setShowPopup(true);
+  }
+
+  function closePopup(){
+    console.log('closePopup');
+    setShowPopup(false);
+  }
+
+  useEffect (() => {
+    if(initialized)
+      return;
+
+    setTimeout (() => {
+      var slider = tns({
+        container: '.product__slider',
+        items: 2,
+        autoWidth: true,
+        gutter: 10,
+        controls: true,
+        nav: false,
+        autoplay: false,
+        mouseDrag: true,
+        arrowKeys: true,
+        controlsContainer: '.product__toggls',
+        prevButton: '.product__arrow--prev',
+        nextButton: '.product__arrow--next',
+        responsive: {
+          900: {
+            items: 4,
+            gutter: 15
+          }
+        }
+      });
+      initialized = true;
+    }, 200)
+  });
 
   return (
     <div className="product__slides">
@@ -48,7 +62,7 @@ export default function Slider({data}) {
         <div className="product__slider">
           {
             data.images.map((value,index) => {
-              return <div key={index} className="product__slide">
+              return <div key={index} className="product__slide" onClick={() => openOverlay(value.url)}>
                 <picture>
                   <source type="image/webp" srcSet={value.url} />
                   <img className="product__slideimg" src={value.url} alt={value.title} />
@@ -58,6 +72,14 @@ export default function Slider({data}) {
           }          
         </div>
       </div>
+      { 
+        showPopup && <Overlay closePopup={() => closePopup()}>
+          <picture>
+            <source type="image/webp" srcSet={selectedImg} />
+            <img className="product__slideimg" src={selectedImg} />
+          </picture>
+        </Overlay>
+      }
     </div>
   )
 }
